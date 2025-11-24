e696ada41dcff231e4f9ae3565322b049f9918ab
itsgreatpro <karahan@academic.rrc.ca>
2025-11-24 10:57:08 -0600

    edited the validation on customers postal code

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,6 +12,6 @@ class Customer < ApplicationRecord
   validates :phone, length: { maximum: 20 }, allow_blank: true
   validates :city, presence: true, length: { maximum: 100 }
   validates :country, presence: true, length: { maximum: 100 }
-  validates :postal_code, presence: true, format: { with: /\A[A-Z]\d[A-Z] ?\d[A-Z]\d\z/i }, allow_blank: true
+  validates :postal_code, presence: true, format: { with: /\A[\w\- ]+\z/ }
   validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
